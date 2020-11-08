ce545979843e896b03253c4d61c793b90ac4ed97
Jacob Evans <cloud887@gmail.com>
2020-11-08 09:36:08 -0600

    save

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -11,16 +11,22 @@ function useToggle() {
   // 🐨 Add a property called `togglerProps`. It should be an object that has
   // `aria-pressed` and `onClick` properties.
   
-  return {on, toggle, togglerProps: {'aria-pressed': on, onClick: toggle}}
+  return {on, toggle, getTogglerProps: ({on = false}) => ({'aria-pressed': on, onClick: toggle})}
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
